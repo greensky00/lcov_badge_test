dcb39b681fedbb054e9ee62564eb74c938efecd7
Jung-Sang Ahn <jungsang.ahn@gmail.com>
2025-04-17 14:35:43 -0700

    fix

--- scripts/test/runtests.sh
+++ scripts/test/runtests.sh
@@ -11,17 +11,3 @@ set -e
 ./tests/new_joiner_test --abort-on-failure
 ./tests/failure_test --abort-on-failure
 
-ASIO_TESTS=(
-    "./tests/timer_test"
-    "./tests/asio_service_test"
-    "./tests/req_resp_meta_test"
-    "./tests/custom_quorum_test"
-    "./tests/stream_transport_layer_test"
-    "./tests/raft_stream_mode_test"
-)
-
-for test in "${ASIO_TESTS[@]}"; do
-    if [ -f $test ]; then
-        $test --abort-on-failure
-    fi
-done
